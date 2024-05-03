f65ce85ece222acb03613798d7a06cff49656137
volesen <vincolesen@gmail.com>
2024-05-03 20:00:34 +0200

    Run prettier on Catalyst components

--- web/src/components/catalyst/link.tsx
+++ web/src/components/catalyst/link.tsx
@@ -1,14 +1,14 @@
-import { DataInteractive as HeadlessDataInteractive } from '@headlessui/react'
-import NextLink, { type LinkProps } from 'next/link'
-import React from 'react'
+import { DataInteractive as HeadlessDataInteractive } from "@headlessui/react";
+import NextLink, { type LinkProps } from "next/link";
+import React from "react";
 
 export const Link = React.forwardRef(function Link(
-  props: LinkProps & React.ComponentPropsWithoutRef<'a'>,
+  props: LinkProps & React.ComponentPropsWithoutRef<"a">,
   ref: React.ForwardedRef<HTMLAnchorElement>
 ) {
   return (
     <HeadlessDataInteractive>
       <NextLink {...props} ref={ref} />
     </HeadlessDataInteractive>
-  )
-})
+  );
+});
