c28dd388cc39858d6c9fb8da0fd4ed3775c7095f
volesen <vincolesen@gmail.com>
2024-05-03 22:07:18 +0200

    Add s3 upload utility

---
+++ web/src/utils/uploadToS3.ts
@@ -0,0 +1,9 @@
+export default function uploadToS3(url: string, file: File) {
+  const formData = new FormData();
+  formData.append("file", file);
+
+  return fetch(url, {
+    method: "PUT",
+    body: formData,
+  });
+}
