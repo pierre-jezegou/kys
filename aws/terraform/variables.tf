c06b0bc137740bcaf2f2cc449bad6e678c508a75
Pierre Jézégou <pierre.jezegou@centrale.centralelille.fr>
2024-05-04 22:52:00 +0200

    S3 (bucket + policies), Lambda functions, Gateway

---
+++ aws/terraform/variables.tf
@@ -0,0 +1,22 @@
+variable "region" {
+  description = "AWS region where resources will be provisioned"
+  type        = string
+}
+
+variable "hosts" {
+  description = "Hosts where the frontend will be hosted"
+  type        = list(string)
+}
+
+variable "lambda_function_names" {
+  description = "Names of the lambda functions"
+  type        = list(string)
+}
+
+variable "api_resources" {
+    type = list(object({
+      name       = string
+      parent     = string
+      path_part  = string
+    }))
+}
