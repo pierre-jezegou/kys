9fb29ea904b2684873a2b11dde0596ac93fda1fe
volesen <vincolesen@gmail.com>
2024-05-04 12:48:24 +0200

    Update root layout

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,3 +1,4 @@
+import clsx from "clsx";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -15,8 +16,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className="h-full bg-gray-100">
+      <body className={clsx("h-full", inter.className)}>{children}</body>
     </html>
   );
 }
