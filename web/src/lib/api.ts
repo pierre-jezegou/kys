16a994854f8fe8366817a7dbad007d1bc9609c27
volesen <vincolesen@gmail.com>
2024-05-04 17:02:57 +0200

    Update create session endpoint

--- web/src/lib/api.ts
+++ web/src/lib/api.ts
@@ -1,19 +1,21 @@
 export const createSession = async (name: string, university: string) => {
   // Extract base endpoint to the environment
-  const response = await fetch(
-    `https://8taeoi4dm2.execute-api.us-east-1.amazonaws.com/get-session-id?name=${name}&university=${university}`,
-    {
-      method: "GET",
-    }
-  );
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/session`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      name,
+      university,
+    }),
+  });
 
   if (!response.ok) {
     throw new Error("Failed to create session");
   }
 
-  // Log the response
-  console.log(response);
-
   const { sessionId } = await response.json();
+
   return sessionId;
 };
