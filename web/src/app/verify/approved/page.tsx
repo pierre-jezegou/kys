1600b4490fc454057a42637d134cadb0a63788f0
volesen <vincolesen@gmail.com>
2024-05-04 11:54:15 +0200

    Remove emojis from title

--- web/src/app/verify/approved/page.tsx
+++ web/src/app/verify/approved/page.tsx
@@ -9,13 +9,19 @@ import Image from "next/image";
 export default function Approved() {
   return (
     <>
-      <DialogTitle>Approved 🎉</DialogTitle>
+      <DialogTitle>Approved</DialogTitle>
       <DialogDescription>
-        Your identity has been successfully verified. We will redirect you back to where you left off.
+        Your identity has been successfully verified. We will redirect you back
+        to where you left off.
       </DialogDescription>
       <DialogBody>
         <div className="flex justify-center my-4">
-          <Image src="/miroodles/color/flower.svg" alt="robot" width="200" height="200" />
+          <Image
+            src="/miroodles/color/flower.svg"
+            alt="robot"
+            width="200"
+            height="200"
+          />
         </div>
       </DialogBody>
     </>
--- web/src/app/verify/denied/page.tsx
+++ web/src/app/verify/denied/page.tsx
@@ -9,14 +9,19 @@ import Image from "next/image";
 export default function Denied() {
   return (
     <>
-      <DialogTitle>Denied 😢</DialogTitle>
+      <DialogTitle>Denied</DialogTitle>
       <DialogDescription>
-        Your identity could not be verified. Please try again or contact support.
-        We will redirect you back to where you left off.
+        Your identity could not be verified. Please try again or contact
+        support. We will redirect you back to where you left off.
       </DialogDescription>
       <DialogBody>
         <div className="flex justify-center my-4">
-          <Image src="/miroodles/color/rain.svg" alt="rain" width="200" height="200" />
+          <Image
+            src="/miroodles/color/rain.svg"
+            alt="rain"
+            width="200"
+            height="200"
+          />
         </div>
       </DialogBody>
     </>
