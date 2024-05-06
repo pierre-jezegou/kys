dbd3b27f23a8d8194b6e69b3aa00d96e7d64f0f4
Pierre Jézégou <pierre.jezegou@centrale.centralelille.fr>
2024-05-06 23:34:47 +0200

    Added unit draft of unit tests

--- api/tests/test_app.py
+++ api/tests/test_app.py
@@ -2,7 +2,42 @@ from chalice.test import Client
 from app import app
 
 
-def test_index():
+def test_create_session():
+    """Test the create session route."""
     with Client(app) as client:
-        response = client.http.get('/')
-        assert response.json_body == {'hello': 'world'}
+        response = client.http.post('/session', json={'name': 'John Doe', 'university': 'UPC'})
+        assert response.status_code == 200
+        assert 'session_id' in response.json_body
+
+def test_get_all_records():
+    """Test the get all records route."""
+    with Client(app) as client:
+        response = client.http.get('/session')
+        assert response.status_code == 200
+        assert isinstance(response.json_body, list) # Be more specific
+
+def test_get_record():
+    """Test the get record route."""
+    with Client(app) as client:
+        session_id = '12345' # WILL NOT WORK: VALID SESSION_ID IS REQUIRED
+        response = client.http.get(f'/session/{session_id}')
+        assert response.status_code == 200
+        assert 'session_id' in response.json_body
+        assert response.json_body['session_id'] == session_id
+
+def test_create_presigned_url():
+    """Test the create presigned URL route."""
+    with Client(app) as client:
+        file_name = 'example.jpg' # TODO IMPORT IMAGE IN TEST FOLDER
+        response = client.http.post(f'/presigned-url/{file_name}')
+        assert response.status_code == 200
+        assert 'presigned_url' in response.json_body
+
+def test_check():
+    """Test the check route."""
+    with Client(app) as client:
+        session_id = '12345' # WILL NOT WORK: VALID SESSION_ID IS REQUIRED
+        response = client.http.get(f'/check/{session_id}')
+        assert response.status_code == 200
+        assert 'session_id' in response.json_body
+        assert response.json_body['session_id'] == session_id
