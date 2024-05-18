5e6af0052002c1c8950af1b58cfc8db9326bff46
Pierre Jézégou <pierre.jezegou@centrale.centralelille.fr>
2024-05-18 11:25:32 +0200

    Resolve wrong place iam injection

--- api/inject-vars.py
+++ api/inject-vars.py
@@ -1,22 +1,26 @@
 import json
 import os
 
-def inject_environment_variables(file_path, env_variables):
+def inject_variables(file_path, env_variables, iam_variable):
     with open(file_path, 'r') as file:
         data = json.load(file)
 
     dev_stage = data['stages']['dev']
     dev_stage['environment_variables'].update(env_variables)
+    dev_stage['iam_role_arn'] = iam_variable['IAM_ROLE_ARN']
 
     with open(file_path, 'w') as file:
         json.dump(data, file, indent=2)
+
 
 if __name__ == "__main__":
     file_path = ".chalice/config.json"
     env_variables = {
         "APP_BUCKET_NAME": os.getenv("APP_BUCKET_NAME", "kys-session-files"),
         "APP_TABLE_NAME": os.getenv("APP_TABLE_NAME", "kys-sessions"),
-        "REGION_NAME": os.getenv("REGION_NAME", "us-east-1"),
+        "REGION_NAME": os.getenv("REGION_NAME", "us-east-1"),   
+    }
+    iam_variable = {
         "IAM_ROLE_ARN": os.getenv("IAM_ROLE_ARN", "")
     }
-    inject_environment_variables(file_path, env_variables)
+    inject_variables(file_path, env_variables, iam_variable)
